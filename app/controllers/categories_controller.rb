edcbe898e4a08aafab71079d8f9bf7a94b144795
tsmith422 <smith.taylor@tamu.edu>
2025-09-15 17:30:50 +0000

    used rubocop to fix minor errors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,19 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
+  def index; end
 
-  def index
-  end
+  def show; end
 
-  def show
-  end
+  def new; end
 
-  def new
-  end
+  def create; end
 
-  def create
-  end
+  def edit; end
 
-  def edit
-  end
+  def update; end
 
-  def update
-  end
+  def delete; end
 
-  def delete
-  end
-
-  def destroy
-  end
-
+  def destroy; end
 end

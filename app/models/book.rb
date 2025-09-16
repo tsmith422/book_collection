a0ca560bf2bc29301bb6640d6ea9261580498b32
tsmith422 <smith.taylor@tamu.edu>
2025-09-16 02:04:45 +0000

    Created User entity; Created User Books entity; Modified views to work with available attributes; Added links to books and users; Set route to the new user books view

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+has_many :users, through: :user_books
+has_many :user_books
+
   validates :title, presence: true
   validates :author, presence: true
   validates :price, numericality: true

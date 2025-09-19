f1bcc1ce67747f0775831cb22b8b47f4028e7a1b
tsmith422 <smith.taylor@tamu.edu>
2025-09-19 16:54:43 +0000

    Started setting up devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  root to: 'dashboards#show'
+  
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
+
   resources :user_books
   resources :users
   root "books#index"
